08e410b9d31abe0b5240c35b1e127a78f504ac83
Alexandr Prokofyev <alprokoviev@gmail.com>
2022-11-06 19:40:19 +0400

    2.16. Начинаем программировать.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type MainScreenProps = {
+  rentalOffers: number;
+}
+
+function App({ rentalOffers }: MainScreenProps): JSX.Element {
+  return <MainScreen rentalOffers={rentalOffers} />;
 }
 
 export default App;
